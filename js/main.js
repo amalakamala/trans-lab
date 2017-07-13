acdb5419675cd33bf0dedbaa77a09cf1557fb7e9
amalakamala <amalakamala.a@gmail.com>
2017-07-13 14:45:00 -0400

    home y profile

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-/* Holiii acá va tu código también */
+$(document).ready(function(){
+	$("#sesion").click(function(event) {
+		if(!(/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/.test($("#mail").val()))){
+   			$("#mail").append($("#mail").val("Error"));
+  		}
+  		if(!(/^\d{8}([0-9])*$/.test($("#contrasena").val())) ){
+   			$("#contrasena").append($("#contrasena").val("Error"));
+  		}
+  		else{
+  			$("#sesion").attr("href","home.html");
+  		}
+	});
+})
