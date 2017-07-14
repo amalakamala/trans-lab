a09f7e422fef219b78d0c0b563f9ad23f3f5f4cd
amalakamala <amalakamala.a@gmail.com>
2017-07-14 00:48:20 -0400

    calcular saldo

--- js/main.js
+++ js/main.js
@@ -9,8 +9,9 @@ $(document).ready(function(){
   $('select').material_select();
 
 
-  /* VALIDACIÓN */
-	$("#sesion").click(function(event) {
+/*-------- VALIDACIÓN INICIAL --------*/
+
+	$("#sesion").click(function(event){
     if(!(/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/.test($("#mail").val()))){
    			$("#mail").append($("#mail").val("Error"));
   		}
@@ -20,44 +21,126 @@ $(document).ready(function(){
   		else{
   			$("#sesion").attr("href","home.html");
         localStorage.setItem('mail',$("#mail").val());
-        localStorage.setItem('num-tarjeta',$("#contrasena").val());
-        
+        localStorage.setItem('num-tarjeta',$("#contrasena").val());       
   		}
 	});
 
+/*-------- PERFIL --------*/
   
   /* DEVOLVER EMAIL en perfil */
   var mail = localStorage.getItem('mail');
   $('#respuesta-correo').html(mail); 
 
   var contrasena = localStorage.getItem('num-tarjeta');
-  $('#tarjetas-creadas').html(contrasena); 
+  console.log(contrasena)
+  $('#tarjetas-creadas').append(`
+    <div class="caja">
+      <h6>`+contrasena+`</h6>
+    </div>
+  `)
+  var arr = [];
 
-  var creadas = localStorage.getItem('todas-tarjetas');
-  $('#tarjetas-creadas').html(creadas);
 
   /* Capturar valor tarjetas */
-  $("#btn-tarjeta").click(function(event) {
+  $("#btn-tarjeta").click(function() {
     if(!(/^\d{8}([0-9])*$/.test($("#tarjeta").val())) ){
         $("#tarjeta").append($("#tarjeta").val("Error"));
     }else{
 
-      localStorage.setItem('num-tarjeta-ing',$("#tarjeta").val()); 
-      var digitos = +localStorage.getItem('num-tarjeta-ing');
+      var valorIngresado = $("#tarjeta").val();
+      arr.push(valorIngresado);
+      console.log(arr);
 
       $("#tarjetas-creadas").append(`
         <div class="caja">
-          <h6>`+digitos+`</h6>
+          <h6>`+valorIngresado+`</h6>
         </div>
       `)
       
-      localStorage.setItem('todas-tarjetas', $('#tarjetas-creadas').html()); 
-      $("#tarjeta").val(""); 
+      localStorage.setItem('todas-tarjetas',arr);
+
+      console.log(localStorage.getItem('todas-tarjetas'));
+
+      console.log(localStorage.getItem('todas-tarjetas', JSON.stringify(arr)));
+      
+      $("#tarjeta").val("");       
     }
 
   });
 
 
+/*-------- LLENAR SELECT --------*/
+
+  var todasTarjetas = localStorage.getItem('todas-tarjetas');
+  console.log(localStorage.getItem('todas-tarjetas'));
+  var str = JSON.parse('[' + todasTarjetas + ']');
+  console.log(str);
+  
+  str.forEach(function(e){
+    $('#el-select').append(`<option value="`+e+`">`+e+`</option>`)
+  });
+
+/*-------- SALDO --------*/
+  $("#tarjeta-saldo").focus(function(){
+    $("#el-select").attr('disabled', '');
+  })
+
+  $("#el-select").focus(function(){
+    $("#tarjeta-saldo").attr('disabled', '');
+    $("#tarjeta-saldo").val("");
+  })
+
+
+
+  $('#btn-ver-saldo').click(function(){
+    $("#tarjeta-saldo").empty();
+    console.log($("#tarjeta-saldo").val());
+
+    if(!(/^\d{8}([0-9])*$/.test($("#tarjeta-saldo").val())) ){
+      $("#tarjeta-saldo").append($("#tarjeta-saldo").val("Error"));
+    }else{     
+      console.log($("#tarjeta-saldo").val());
+      var valorTarjeta = $("#tarjeta-saldo").val();
+      callbacksAjaxBip(valorTarjeta);
+      $("#tarjeta-saldo").val("");
+    }
+  });
+
+  $('#btn-ver-saldo').click(function(){
+    $("#tarjeta-saldo").empty();
+    if($("#el-select").val() == ""){
+      alert("Escoge una opción")
+    }else{      
+      console.log($("#el-select").val());
+      var valorTarjetaS = $("#el-select").val();
+      callbacksAjaxBip(valorTarjetaS);
+    }
+  });
+
+  var callbacksAjaxBip = function(num){
+    $.ajax({
+      url: 'http://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip=' + num,
+      type: 'GET',
+      dataType: 'json',
+    })
+    .done(function(response){    
+      var elSaldo = response.saldoTarjeta; 
+      $("#saldo").html(`
+        <div class="caja-saldo">
+          <div class="row">
+            <div class="col s12 center total-text">
+              <h6>SALDO TOTAL</h6>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col s12 center saldo-text">
+              <h4>`+elSaldo+`</h4>
+            </div>
+          </div>
+        </div> 
+      `);
+    })  
+  }  
 
 
 })
