a03e4ff6ee65db80778b19e53babd3eac4cc8c80
amalakamala <amalakamala.a@gmail.com>
2017-07-13 21:09:26 -0400

    saldo

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+  /* NAV */
+  $(".button-collapse").sideNav();
+  /* COLLAPSIBLE */
+  $('.collapsible').collapsible();
+  /* SELECT */
+  $('select').material_select();
+
+
   /* VALIDACIÓN */
 	$("#sesion").click(function(event) {
     if(!(/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/.test($("#mail").val()))){
@@ -15,28 +23,25 @@ $(document).ready(function(){
   		}
 	});
 
-  /* NAV */
-  $(document).ready(function(){
-    $(".button-collapse").sideNav();
-  })
-
+  
   /* DEVOLVER EMAIL en perfil */
   var mail = localStorage.getItem('mail');
   $('#respuesta-correo').html(mail); 
 
   var contrasena = localStorage.getItem('num-tarjeta');
-  $('#tarjetas-creadas').html(contrasena);
-  
+  $('#tarjetas-creadas').html(contrasena); 
+
   var creadas = localStorage.getItem('todas-tarjetas');
   $('#tarjetas-creadas').html(creadas);
-
 
   /* Capturar valor tarjetas */
   $("#btn-tarjeta").click(function(event) {
     if(!(/^\d{8}([0-9])*$/.test($("#tarjeta").val())) ){
         $("#tarjeta").append($("#tarjeta").val("Error"));
     }else{
-      var digitos = localStorage.getItem('num-tarjeta-ing');
+
+      localStorage.setItem('num-tarjeta-ing',$("#tarjeta").val()); 
+      var digitos = +localStorage.getItem('num-tarjeta-ing');
 
       $("#tarjetas-creadas").append(`
         <div class="caja">
@@ -44,10 +49,10 @@ $(document).ready(function(){
         </div>
       `)
       
+      localStorage.setItem('todas-tarjetas', $('#tarjetas-creadas').html()); 
       $("#tarjeta").val(""); 
     }
 
-    localStorage.setItem('todas-tarjetas',$("#tarjetas-creadas").html());
   });
 
 
